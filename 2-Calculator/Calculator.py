1584d60c5b87003fe4367c44cf20cfc6ec1395d1
Mona Lisa <slavi.nedqlkov@abv.bg>
2023-03-26 22:42:54 +0300

    New Calculator Project

---
+++ 2-Calculator/Calculator.py
@@ -0,0 +1,22 @@
+print("Please enter values for X and Y")
+print("-------------------------------")
+print("Enter x: ")
+x = input()
+print("Enter y: ")
+y = input()
+print("Please choose an arithmetic operation\nA) Addition(+)\nB) Substraction(-)\nC) Multiplication(*)\nD) Division(/)")
+arithmetic_operation = input(str())
+if arithmetic_operation == "A":
+	print(int(x) + int(y))
+elif arithmetic_operation == "B":
+	print(int(x) - int(y))
+elif arithmetic_operation == "C":
+	print(int(x) * int(y))
+elif arithmetic_operation == "D":
+	print(float(x) / float(y))
+
+
+
+
+
+
