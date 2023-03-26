241c93b1fb7bd984c22d1855b3b44838fc8d3c54
Mona Lisa <slavi.nedqlkov@abv.bg>
2023-03-26 22:47:44 +0300

    Calculator New Modulo Feature

--- 2-Calculator/Calculator.py
+++ 2-Calculator/Calculator.py
@@ -4,7 +4,7 @@ print("Enter x: ")
 x = input()
 print("Enter y: ")
 y = input()
-print("Please choose an arithmetic operation\nA) Addition(+)\nB) Substraction(-)\nC) Multiplication(*)\nD) Division(/)")
+print("Please choose an arithmetic operation\nA) Addition(+)\nB) Substraction(-)\nC) Multiplication(*)\nD) Division(/)\nC) Check Remainder(%)")
 arithmetic_operation = input(str())
 if arithmetic_operation == "A":
 	print(int(x) + int(y))
@@ -14,6 +14,8 @@ elif arithmetic_operation == "C":
 	print(int(x) * int(y))
 elif arithmetic_operation == "D":
 	print(float(x) / float(y))
+elif arithmetic_operation == "E":
+	print(int(x) % int(y))
 
 
 
