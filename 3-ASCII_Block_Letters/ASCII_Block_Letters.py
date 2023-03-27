8eb3b0b1ba6b2a4408f08666b661ba389cc50e67
Mona Lisa <slavi.nedqlkov@abv.bg>
2023-03-27 21:11:36 +0300

    ASCII Block Letter program

---
+++ 3-ASCII_Block_Letters/ASCII_Block_Letters.py
@@ -0,0 +1,33 @@
+
+S = """
+ SSSS
+S    
+  SSS
+     S
+SSSS 
+"""
+T = """
+TTTTTTTT
+   T
+   T
+   T
+   T
+"""
+A = """
+    A  
+  A   A
+  A    A
+ AAAAAAAA
+ A      A
+ A      A
+ A      A
+ """
+ N = """
+ N    N
+ NN   N
+ N  N N
+ N   NN
+ N    N
+ N    N
+ """
+ 
